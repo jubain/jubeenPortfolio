75474fc7f37e4d12a1aadbf51a096c2479f4a358
jubain <jubeennp@gmail.com>
2021-11-07 22:38:31 +0000

    change in testimonial page

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -35,7 +35,7 @@ function Testimonial(props) {
                                 <h3 className="vertical-timeline-element-title">{element.title}</h3>
                                 <h5 className="vertical-timeline-element-subtitle">{element.location}</h5>
                                 <p id="description">{element.description}</p>
-                                {showButton && (<a className={`button ${isWorkIcon ? "workButton" : "schoolButton"}`} href="/">{element.buttonText}</a>)}
+                                
 
                             </VerticalTimelineElement>
                         )
