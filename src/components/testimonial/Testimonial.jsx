01d4d03e176f1e06bfdc0c290371864cffb53600
Jubeen Amatya <45592860+jubain@users.noreply.github.com>
2022-06-03 15:16:14 +0100

    fixing

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "./timeline.css";
 import SchoolIcon from "@mui/icons-material/School";
@@ -27,31 +28,31 @@ function Testimonial(props) {
     <div className="timeline" id="testimonial">
       <h1 className="heading title">TIMELINE</h1>
       <VerticalTimeline className="timelineContainer">
-      {timeLine.map((element) => {
-        let isWorkIcon = element.icon === "Work";
-        let showButton =
-          element.buttonText !== undefined &&
-          element.buttonText !== null &&
-          element.buttonText !== "";
+        {timeLine.map((element) => {
+          let isWorkIcon = element.icon === "Work";
+          let showButton =
+            element.buttonText !== undefined &&
+            element.buttonText !== null &&
+            element.buttonText !== "";
 
-        return (
-          <VerticalTimelineElement
+          return (
+            <VerticalTimelineElement
               key={element.id}
               date={element.date}
               className="vertical-timeline-element--work verticalTimeline"
               dateClassName="date"
               icon={isWorkIcon ? <WorkIcon fontSize="12" /> : <SchoolIcon />}
-              contentStyle={{ backgroundColor:'white' , color: '#15023a' }}
-              contentArrowStyle={{ borderLeft: '7px solid  rgb(33, 150, 243)',color:'white' }}
+              contentStyle={{}}
+              contentArrowStyle={{ borderLeft: '7px solid  rgb(33, 150, 243)', color: 'white' }}
               iconStyle={isWorkIcon ? workIconStyle : schoolIconStyle}
               iconClassName="icon"
-          >
+            >
               <h3 className="vertical-timeline-element-title">{element.title}</h3>
               <h5 className="vertical-timeline-element-subtitle">{element.location}</h5>
               <p id="description">{element.description}</p>
-          </VerticalTimelineElement>
-        );
-      })}
+            </VerticalTimelineElement>
+          );
+        })}
       </VerticalTimeline>
     </div>
   );
